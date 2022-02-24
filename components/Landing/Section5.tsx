1d87bf1713f04e32234b4aea227859fc0a61177a
js30703 <js01053597029@gmail.com>
2022-02-24 15:59:13 +0700

    lg-opti2

--- components/Landing/Section5.tsx
+++ components/Landing/Section5.tsx
@@ -16,8 +16,8 @@ export default function Section5() {
         bg="url('images/wp-content/service-6.jpg') no-repeat top 20px left 20px /320px 500px"
         opacity={0.98}
       >
-        <Flex pos="relative" ml="auto" mt="auto" w="320px" h="512px" borderBottomRadius="200px">
-          <NextImage src={bg} borderBottomRadius="200px" alt="drg_inn" priority quality={50} />
+        <Flex pos="relative" ml="auto" mt="auto" w="370px" h="512px" borderBottomRadius="200px">
+          <NextImage src={bg} borderBottomRadius="200px" alt="drg_inn" quality={50} />
         </Flex>
       </Flex>
       <VStack w="100%" spacing="20px" p="10px">
