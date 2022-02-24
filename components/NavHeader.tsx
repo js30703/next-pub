453a07bfb1cda0f84a8c15bf3c34a605078454f7
js30703 <js01053597029@gmail.com>
2022-02-24 07:59:04 +0700

    imgae opti

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -1,11 +1,13 @@
-import { Box, ChakraProps, Image } from '@chakra-ui/react';
+import { Box, ChakraProps } from '@chakra-ui/react';
 import React from 'react';
+import Image from 'next/image';
+import logo from 'public/images/logo.jpg';
 type NavHeaderProps = ChakraProps;
 
 export default function NavHeader({ ...props }: NavHeaderProps) {
   return (
     <Box w="100%" p="5px 20px" h="66px" {...props}>
-      <Image w="120px" src="images/logo.jpg" />
+      <Image width="120px" height="60px" src={logo} alt="logo" />
     </Box>
   );
 }
