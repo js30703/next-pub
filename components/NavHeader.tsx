adb5fe36fb197bb6e5d13144de83670f97a1d936
js30703 <js01053597029@gmail.com>
2022-02-24 14:57:25 +0700

    no lazy3

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -7,7 +7,7 @@ type NavHeaderProps = ChakraProps;
 export default function NavHeader({ ...props }: NavHeaderProps) {
   return (
     <Box w="100%" p="5px 20px" h="66px" {...props}>
-      <Image width="120px" height="60px" src={logo} alt="logo" />
+      <Image width="120px" height="60px" src={logo} alt="logo" priority />
     </Box>
   );
 }
